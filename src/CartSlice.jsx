2ff8af051fee03530c7346c248325f9fc1009ca5
Your Name <you@example.com>
2025-06-17 05:07:19 -0400

    T

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,11 +3,21 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
+    
     items: [], // Initialize items as an empty array
   },
+  
   reducers: {
     addItem: (state, action) => {
-    
+        const {name,image,cost}=action.payload;
+const existingItem=state.items.find((items)=> items.name===name)
+
+if(existingItem){
+        existingItem.quantity++; }
+    else {
+        state.items.push({name, image, cost, quantity: 1 });
+    }
+
     },
     removeItem: (state, action) => {
     },
